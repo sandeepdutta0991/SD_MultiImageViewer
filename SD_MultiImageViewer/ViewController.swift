edc40b573e82bec3a784acc2c5b2e7e90b1e3717
Sandeep Dutta <sandeepdutta0991@gmail.com>
2019-09-07 12:45:17 +0530

    Initial Commit

---
+++ SD_MultiImageViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SD_MultiImageViewer
+//
+//  Created by SANDEEP DUTTA on 07/09/19.
+//  Copyright © 2019 SANDEEP DUTTA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SD_MultiImageViewerTests/SD_MultiImageViewerTests.swift
@@ -0,0 +1,34 @@
+//
+//  SD_MultiImageViewerTests.swift
+//  SD_MultiImageViewerTests
+//
+//  Created by SANDEEP DUTTA on 07/09/19.
+//  Copyright © 2019 SANDEEP DUTTA. All rights reserved.
+//
+
+import XCTest
+@testable import SD_MultiImageViewer
+
+class SD_MultiImageViewerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
